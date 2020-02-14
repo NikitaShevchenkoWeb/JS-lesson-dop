5b2fffa82f873cea51272dbb69f41e21d9697ee9
Nikita Shevchenko <nikita_shevchenko@inbox.ru>
2020-02-14 21:23:04 +0300

    Lesson 6 v2

--- dev/js/main.js
+++ dev/js/main.js
@@ -4,33 +4,47 @@ let isNumber = function(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-function one() {
-    let a = 10;
+function one(newCount) {
+    let a = 10,
+        count = newCount;
 
-    let userNum = prompt("Угадай число от 1 до 100");
+    let userNum = prompt("Угадай число от 1 до 100. Попыток: " + count);
 
     function two(b) {
+        let newCount;
+
         if (b == null) {
             alert('Game Over');
         }
         else if (!isNumber(b)) {
             alert("Введи число!");
-            one();
+            one(count);
+        }
+        else if (count == 0) {
+            alert("Попытки закончились, хотите сыграть еще?");
+            if (true) {
+                one(10);
+            }
         }
         else if (b > a) {
-            alert("Загаданное число меньше");
-            one();
+            newCount = count - 1;
+            alert("Загаданное число меньше,осталось попыток: " + newCount);
+            one(newCount);
         }
         else if (b < a) {
-            alert("Загаданное число больше");
-            one();
+            newCount = count - 1;
+            alert("Загаданное число больше,осталось попыток: " + newCount);
+            one(newCount);
         }
         else if (b == a) {
-            alert("Вы угадали!");
+            alert("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+            if (true) {
+                one(10);
+            }
         }
     }
 
     two(userNum);
 }
 
-one();
+one(10);
